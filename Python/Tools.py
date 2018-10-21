818650e815ca48c85f348365104e93d20a718de6
peteparkinson <43157092+peteparkinson@users.noreply.github.com>
2018-10-21 17:21:55 -0400

    Update Tools.py

--- Python/Tools.py
+++ Python/Tools.py
@@ -3,7 +3,6 @@ import math
 ################################################
 """
 check if specific number is prime
-this needs fixed
 """
 
 
@@ -105,62 +104,3 @@ def is_palindrome(num):
     if num == num[::-1]:
         return True
     return False
-
-
-################################################
-"""
-Sieve of Atkin
-This code is contributed by Smitha
-not working
-"""
-
-
-def sieve(limit):
-    primes = []
-
-    # 2 and 3 are known to be prime
-    if limit > 2:
-        primes.append(2)
-    if limit > 3:
-        primes.append(3)
-
-    # Initialise the sieve array with False values
-    sieve = [False] * limit
-    for i in range(0, limit):
-        sieve[i] = False
-
-    x = 1
-    while x * x < limit:
-        y = 1
-        while y * y < limit:
-
-            # Main part of Sieve of Atkin
-            n = (4 * x * x) + (y * y)
-            if (n <= limit and (n % 12 == 1 or
-                                n % 12 == 5)):
-                sieve[n] ^= True
-
-            n = (3 * x * x) + (y * y)
-            if n <= limit and n % 12 == 7:
-                sieve[n] ^= True
-
-            n = (3 * x * x) - (y * y)
-            if (x > y and n <= limit and
-                    n % 12 == 11):
-                sieve[n] ^= True
-            y += 1
-        x += 1
-
-    # Mark all multiples of squares as non-prime
-    r = 5
-    while r * r < limit:
-        if sieve[r]:
-            for i in range(r * r, limit, r * r):
-                sieve[i] = False
-
-    # populate array
-    for a in range(5, limit):
-        if sieve[a]:
-            primes.append(a)
-
-    return primes
