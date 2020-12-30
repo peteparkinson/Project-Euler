a62279adceb6f6f0a2c7b8d44981aac1a7e59238
peteparkinson <43157092+peteparkinson@users.noreply.github.com>
2020-12-30 13:57:24 -0500

    Update Problem018.java

--- Java/src/Problem018.java
+++ Java/src/Problem018.java
@@ -1,79 +1,108 @@
-import java.awt.List;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+/* Problem 18
+ * By starting at the top of the triangle below and moving to adjacent 
+ * numbers on the row below, the maximum total from top to bottom is 23.
 
-/* Project Euler, problem 18
- * 
- * By starting at the top of the triangle below and moving to adjacent numbers on the row below,
- * the maximum total from top to bottom is 23.
- * 
- * 			   3
- * 			  7 4
- * 			 2 4 6
- * 			8 5 9 3
- * 
+	   3
+	  7 4
+	 2 4 6
+	8 5 9 3
+
  * That is, 3 + 7 + 4 + 9 = 23.
- * 
  * Find the maximum total from top to bottom of the triangle below:
- * 
- * 					       75
- * 				         95 64
- * 				        17 47 82
- * 				      18 35 87 10
- * 				     20 04 82 47 65
- * 			       19 01 23 75 03 34
- * 			      88 02 77 73 07 63 67
- * 			    99 65 04 28 06 16 70 92
- * 			   41 41 26 56 83 40 80 70 33
- * 		     41 48 72 33 47 32 37 16 94 29
- * 		    53 71 44 65 25 43 91 52 97 51 14
- *	      70 11 33 28 77 73 17 78 39 68 17 57
- * 	     91 71 52 38 17 14 91 43 58 50 27 29 48
- * 	   63 66 04 68 89 53 67 30 73 16 69 87 40 31
- * 	  04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
- * 
- * NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route.
- * However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot
- * be solved by brute force, and requires a clever method! ;o)
- * 
- * no answer found yet.
+
+			    75
+			  95  64
+			17  47  82
+		      18  35  87  10
+		    20  04  82  47  65
+		  19  01  23  75  03  34
+		88  02  77  73  07  63  67
+	      99  65  04  28  06  16  70  92
+	    41  41  26  56  83  40  80  70  33
+	  41  48  72  33  47  32  37  16  94  29
+	53  71  44  65  25  43  91  52  97  51  14
+      70  11  33  28  77  73  17  78  39  68  17  57
+    91  71  52  38  17  14  91  43  58  50  27  29  48
+  63  66  04  68  89  53  67  30  73  16  69  87  40  31
+04  62  98  27  23  09  70  98  73  93  38  53  60  04  23
+
+ * NOTE: As there are only 16384 routes, it is possible to solve this 
+ * problem by trying every route. However, Problem 67, is the same 
+ * challenge with a triangle containing one-hundred rows; it cannot be 
+ * solved by brute force, and requires a clever method! ;o)
+ *
+ * // start at the bottom, compare each to its neighbor and add the
+ * // greater of the two to the line above.  continue all the way up
+ *
+ * ans = 1074;
  */
 
+package projectEuler;
+
 public class Problem018 {
+	public static void main(String[] args) {
+		
+		int[][] arr = new int[][] 
+			{
+				{75,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0}, 
+				{95, 64,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0}, 
+				{17, 47, 82,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0}, 
+				{18, 35, 87, 10,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0}, 
+				{20,  4, 82, 47, 65,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0}, 
+				{19,  1, 23, 75,  3, 34,  0,  0,  0,  0,  0,  0,  0,  0,  0}, 
+				{88,  2, 77, 73,  7, 63, 67,  0,  0,  0,  0,  0,  0,  0,  0}, 
+				{99, 65,  4, 28,  6, 16, 70, 92,  0,  0,  0,  0,  0,  0,  0}, 
+				{41, 41, 26, 56, 83, 40, 80, 70, 33,  0,  0,  0,  0,  0,  0}, 
+				{41, 48, 72, 33, 47, 32, 37, 16, 94, 29,  0,  0,  0,  0,  0}, 
+				{53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14,  0,  0,  0,  0}, 
+				{70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57,  0,  0,  0}, 
+				{91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48,  0,  0}, 
+				{63, 66,  4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31,  0}, 
+				{ 4, 62, 98, 27, 23,  9, 70, 98, 73, 93, 38, 53, 60,  4, 23}
+			};
 
-	static int[][] num = new int[15][15];
-	
-	public static void main(String[] args) throws Exception{
-		
-		File file = new File("C:\\Users\\037103\\workspace\\Euler\\Problem018.txt");
-		Scanner sc = new Scanner(file);
-		
-		int answer = 1;
-		
-		for (int j = 0; sc.hasNextLine(); j++) {
-			String[] tmp = sc.nextLine().split(" ");
-			for (int i = 0; i < tmp.length; i++) {
-				num[j][i] = Integer.valueOf(tmp[i]);
+		for(int a = arr[0].length - 2; a >= 0; a--) {
+			for(int b = 0; b < arr.length; b++) {
+				if(arr[a][b] != 0) {
+					if(arr[a+1][b] > arr[a+1][b+1]) {
+						arr[a][b] += arr[a+1][b];
+					} else {
+						arr[a][b] += arr[a+1][b+1];
+					}
+				}
 			}
 		}
-		
-		for (int i = 0; i < 15; i++) {
-			
-			//answer *= 
-			
-		}
-		
-		for (int i = 0; i < num.length; i++) {
-
-			System.out.println();
-			for (int j = 0; j < num[0].length; j++) {
-				
-				System.out.print(num[i][j] + " ");
-			}
-		}
+		System.out.println(arr[0][0]);
 	}
-	
 }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
